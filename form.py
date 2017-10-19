7135b23497389d4491bfafa33632cbe0ad28004c
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2017-10-19 14:20:59 +0100

    Adds game files to project

---
+++ form.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+class ReuseableForm(Form):
+    guess = TextField('Guess: ', validators=[validators.required()])
+
+@app.route("/", methods=['GET', 'POST'])
+
+def hello():
+    form = ReuseableForm(request.form)
+
+    print(form.errors)
+    if request.method == 'POST':
+        guess = request.form['guess']
+        print(guess)
+
+        if form.validate():
+            response = check(guess)
+            form = ReuseableForm()
+            flash(response)
+        else:
+            flash('Please enter a valid number')
+
+    return render_template('hello.html', form=form)
+
+def check(y):
+    x = 4
+    if int(y) == x:
+        return 'Well done!'
+    if int(y) > x:
+        return 'Guess a little lower'
+    if int(y) < x:
+        return 'Guess a little higher'
+
+
+if __name__ == '__main__':
+    app.run()
